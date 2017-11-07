b0c9b6f2ac50953ee8e7c32a40da48be8abab3b0
richietjin <richietjin@gmail.com>
2017-11-07 12:44:24 +0100

    get functions in get result function

--- api.py
+++ api.py
@@ -6,50 +6,45 @@ url = "https://webservices.ns.nl/ns-api-stations-v2"
 naamEnWachtwoord = HTTPBasicAuth("richietjin@gmail.com", "JAavldI8fR8iwjT540mjS4TuJ4d4BwFwbDkhV3SVvJZcqa0kX0a3Xg")
 response = requests.post(url, auth=naamEnWachtwoord)
 
-print(response)
-print(response.content)
-
 xmltodict = xmltodict.parse(response.content)
-print(xmltodict)
 
 stationCodes = []
 
-def get_all_stations():
-    for stations in xmltodict['Stations']['Station']:
-        stations_namen = stations['Namen']['Lang']
-        return stations_namen
-
-def beginstation():
-    stations = get_all_stations()
+def beginstation(stations_namen):
     while True:
        beginstation = str(input('Wat is je beginstation? : '))
-       if beginstation in stations:
+       if beginstation in stations_namen:
            return beginstation
        else:
            print('Verkeerde invoer')
 
-def eindstation():
+def eindstation(stations_namen):
     while True:
        eindstation = str(input('Wat is je eindstation? : '))
-       for station in xmltodict['Stations']['Station']:
-           if eindstation in station['Namen']['Lang']:
-               return eindstation
-           else:
-                print('verkeerde invoer')
+       if eindstation in stations_namen:
+           return eindstation
+       else:
+            print('verkeerde invoer')
 
-def tijden(beginstation, eindstation):
-    import xmltodict
+# def tijden(beginstation, eindstation):
+#     import xmltodict
+#
+#
+#     response = requests.post(url_tijden, auth=naamEnWachtwoord)
+#
+#     print(response)
+#     print(response.content)
+#
+#     xmltodict = xmltodict.parse(response.content)
+#     print(xmltodict)
 
-    url_tijden = "http://webservices.ns.nl/ns-api-treinplanner?fromStation=" + str(beginstation) + "&toStation=" + str(eindstation) + "&departure=true"
+def get_result():
+    stations_namen = []
+    for stations in xmltodict['Stations']['Station']:
+        stations_namen.append(stations['Namen']['Lang'])
 
-    response = requests.post(url_tijden, auth=naamEnWachtwoord)
+    url_tijden = "http://webservices.ns.nl/ns-api-treinplanner?fromStation=" + str(beginstation(stations_namen)) + "&toStation=" + str(beginstation(stations_namen)) + "&departure=true"
 
-    print(response)
-    print(response.content)
+    print(url_tijden)
 
-    xmltodict = xmltodict.parse(response.content)
-    print(xmltodict)
-
-# tijden(beginstation(), eindstation())
-# beginstation(get_all_stations())
-beginstation()
+get_result()
